8fdf53aa8c2bafe07294b33d141e9b5e011384b7
x4w1ch <46028607+x4w1ch@users.noreply.github.com>
2020-03-23 21:16:43 +0200

    Add files via upload

---
+++ Lab_1/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Calculator : Form
+    {
+        double left_operand = 0;
+        double right_operand = 0;
+        Operation operation;
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Number_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text += (sender as Button).Text;
+        }
+
+        private void Clean_Button_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+        }
+
+        private void Plus_Button_Click(object sender, EventArgs e)
+        {
+            this.operation = Operation.Plus;
+            this.left_operand = double.Parse(this.textBox1.Text);
+            this.textBox1.Text = "";
+        }
+        private void Minus_Button_Click(object sender, EventArgs e)
+        {
+            this.operation = Operation.Minus;
+            this.left_operand = double.Parse(this.textBox1.Text);
+            this.textBox1.Text = "";
+        }
+        private void Multiply_Button_Click(object sender, EventArgs e)
+        {
+            this.operation = Operation.Multiply;
+            this.left_operand = double.Parse(this.textBox1.Text);
+            this.textBox1.Text = "";
+        }
+        private void Devide_Button_Click(object sender, EventArgs e)
+        {
+            this.operation = Operation.Devide;
+            this.left_operand = double.Parse(this.textBox1.Text);
+            this.textBox1.Text = "";
+        }
+
+        private void Equal_Button_Click(object sender, EventArgs e)
+        {
+            this.right_operand = double.Parse(this.textBox1.Text);
+            switch (this.operation)
+            {
+                case Operation.Plus:
+                    this.textBox1.Text = (left_operand + right_operand).ToString();
+                    break;
+                case Operation.Minus:
+                    this.textBox1.Text = (left_operand - right_operand).ToString();
+                    break;
+                case Operation.Multiply:
+                    this.textBox1.Text = (left_operand * right_operand).ToString();
+                    break;
+                case Operation.Devide:
+                    this.textBox1.Text = (left_operand / right_operand).ToString();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        enum Operation
+        {
+            Plus,
+            Minus,
+            Multiply,
+            Devide
+        }
+    }
+}
---
+++ Lab_2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Lab_2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Lab_2")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("f418c494-a52f-463c-a82c-a32e5d48d9b3")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
