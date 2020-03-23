b05b0b301706bcfd688de359452e985d74c53ab5
x4w1ch <46028607+x4w1ch@users.noreply.github.com>
2020-03-23 21:48:30 +0200

    Add files via upload

---
+++ 2_Semestr/Lab_2/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Timer1.Interval = 200;
+            this.Timer1.Tick += Timer1_Tick;
+            this.Timer1.Start();
+        }
+
+        string text = "12345";
+        bool on_or_off = true;
+        int i = 0;
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
